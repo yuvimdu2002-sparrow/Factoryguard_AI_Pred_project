bbb877e7d55a020a09d8c45371fc74a4a0d95c4c
yuvimdu2002-sparrow <yuvimdu2002@gmail.com>
2026-01-29 21:11:43 +0530

    Add Shap and model Evaluation file

---
+++ src/Shap.py
@@ -0,0 +1,49 @@
+﻿import joblib
+import shap
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/processed/features_engineering_output.csv")
+
+X = df.drop(["failure_in_next_24h"], axis=1)
+
+# Load FINAL production model
+model = joblib.load("model/xgboost_tuned.joblib")
+
+# SHAP Explainer (Tree-based)
+
+explainer = shap.TreeExplainer(model)
+
+background = X.sample(500, random_state=42)
+
+shap_values = explainer.shap_values(background)
+
+# GLOBAL EXPLANATION
+
+plt.figure()
+shap.summary_plot(shap_values, background, show=False)
+plt.tight_layout()
+plt.savefig("report/shap_global_feature_importance.png")
+plt.close()
+
+print("Global SHAP summary saved")
+
+# LOCAL EXPLANATION (single failure case)
+# Pick a row where model predicts high failure probability
+
+failure_case = X.iloc[[0]]  # replace index with an actual failure if needed
+
+shap_value_single = explainer.shap_values(failure_case)
+
+shap.force_plot(
+    explainer.expected_value,
+    shap_value_single,
+    failure_case,
+    matplotlib=True,
+    show=False
+)
+
+plt.savefig("report/shap_local_failure_explanation.png")
+plt.close()
+
+print("Local SHAP explanation saved")
