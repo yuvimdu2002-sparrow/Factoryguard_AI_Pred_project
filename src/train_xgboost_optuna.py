79ad3838c291036edc659becbd0f344ff5ed8327
yuvimdu2002-sparrow <yuvimdu2002@gmail.com>
2026-02-01 01:20:22 +0530

    Modify the xgboost model with pr_auc score

--- src/train_xgboost_optuna.py
+++ src/train_xgboost_optuna.py
@@ -2,11 +2,13 @@ import optuna
 import joblib
 import pandas as pd
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, average_precision_score
 
-df = pd.read_csv("data/processed/features_engineering_output.csv")
+#  LOAD DATA
+df = pd.read_csv("/content/drive/MyDrive/Zaalima project/features_engineering_output.csv")
 
-X = df.drop(["failure_in_next_24h", "arm_id", "timestamp"], axis=1)
+X = df.drop(["failure_in_next_24h"], axis=1)
 y = df["failure_in_next_24h"]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -14,6 +16,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 scale_pos_weight = y_train.value_counts()[0] / y_train.value_counts()[1]
+
+# CROSS VALIDATION
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+# OPTUNA OBJECTIVE
 
 def objective(trial):
     model = XGBClassifier(
@@ -27,12 +34,21 @@ def objective(trial):
         random_state=42
     )
 
-    model.fit(X_train, y_train)
-    return model.score(X_train, y_train)  # training-only
+    scores = cross_val_score(
+        model,
+        X_train,
+        y_train,
+        cv=cv,
+        scoring="average_precision"   # PR-AUC
+    )
 
+    return scores.mean()
+
+# OPTUNA SEARCH
 study = optuna.create_study(direction="maximize")
 study.optimize(objective, n_trials=30)
 
+# FINAL MODEL
 best_model = XGBClassifier(
     **study.best_params,
     scale_pos_weight=scale_pos_weight,
@@ -42,5 +58,15 @@ best_model = XGBClassifier(
 
 best_model.fit(X_train, y_train)
 
-joblib.dump(best_model, "model/xgboost_tuned.joblib")
-print(" XGBoost trained with Optuna")
+#  EVALUATION
+y_pred = best_model.predict(X_test)
+y_probs = best_model.predict_proba(X_test)[:, 1]
+
+print("Accuracy :", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("PR-AUC   :", average_precision_score(y_test, y_probs))
+
+# SAVE MODEL
+joblib.dump(best_model, "/content/drive/MyDrive/Zaalima project/xgboost_tuned.joblib")
+
+print("XGBoost trained with Optuna + Cross Validation")
