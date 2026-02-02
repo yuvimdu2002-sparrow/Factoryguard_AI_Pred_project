967aa83f43724ec6fea110a0f154acf2b64b06d8
yuvimdu2002-sparrow <yuvimdu2002@gmail.com>
2026-02-03 02:15:45 +0530

    Modify the feauture engineering part

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import joblib
 
-SENSORS = ['temperature_c', 'vibration_rms_mm_s', 'pressure_bar']
-WINDOWS = [1, 6, 12]
-
-def feature_engineering(input_csv):
-    df = pd.read_csv(input_csv)
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df = df.sort_values(['arm_id', 'timestamp'])
-
+df = pd.read_csv("data/raw/factoryguard_synthetic_500.csv")
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df = df.sort_values(['arm_id', 'timestamp'])
+df["failure_in_next_24h"] = (df["time_to_failure_hours"].notna() &(df["time_to_failure_hours"] <= 24)).astype(int)
+print(df["failure_in_next_24h"].value_counts())
+    
+def feature_engineering(SENSORS,WINDOWS):
+    global df
     for sensor in SENSORS:
         for w in WINDOWS:
             df[f'{sensor}_roll_mean_{w}h'] = (
@@ -28,30 +28,27 @@ def feature_engineering(input_csv):
 
         df[f'{sensor}_lag_1'] = df.groupby('arm_id')[sensor].shift(1)
         df[f'{sensor}_lag_2'] = df.groupby('arm_id')[sensor].shift(2)
+        
 
     df = df.groupby('arm_id').apply(lambda x: x.iloc[12:]).reset_index(drop=True)
     
-    df["failure_in_next_24h"] = (df["time_to_failure_hours"].notna() &(df["time_to_failure_hours"] <= 24)).astype(int)
-    print(df["failure_in_next_24h"].value_counts())
+    return df
 
-    print(df.columns)
-    print(df.isna().sum())
-    print(df.info())
-    print(df.describe())
+
+
+if __name__ == "__main__":
+    feature_engineering(['temperature_c', 'vibration_rms_mm_s', 'pressure_bar'],[1, 6, 12])
+    joblib.dump(df, "model/features_engineering.joblib")
 
     # drop columns with the help of feature selection report
     drop_features=["arm_id","timestamp","time_to_failure_hours","pressure_bar_roll_std_1h","vibration_rms_mm_s_roll_std_1h","rpm",
                 "temperature_c_roll_std_1h","error_count","load_pct","maintenance_days_ago","humidity_pct","age_hours"]
     df.drop(drop_features, axis=1, inplace=True)
-    print(df)
-
-    return df
-
-
-if __name__ == "__main__":
-    df = feature_engineering("data/raw/factoryguard_synthetic_500.csv")
+    print(df.columns)
+    print(df.isna().sum())
+    print(df.info())
+    print(df.describe())
 
     df.to_csv("data/processed/features_engineering_output.csv", index=False)
-    joblib.dump(df, "model/features_engineering.joblib")
-
+    
     print("Feature engineering completed")
